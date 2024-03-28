e51ff4d7845c0199b5b9b0d387beeefd448c6665
sarthak25sharma <155518490+sarthak25sharma@users.noreply.github.com>
2024-03-28 14:28:28 +0530

    Hero Div

---
+++ Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.css"
+function Hero () {
+
+    return(
+        <>
+            <div className="hero-image">
+                <img  className="heroimg" src="./public/Hero.jpg" alt="samtech"></img>
+                <h1 className="text-overlay">WE ARE SAMTECH</h1>
+            </div>    
+        </>
+    );
+}
+export default Hero;
