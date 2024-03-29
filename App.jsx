951e89cff7aa5939a285710356a31f06ce33e6d9
sarthak25sharma <155518490+sarthak25sharma@users.noreply.github.com>
2024-03-29 12:57:43 +0530

    App and Main JSX files

---
+++ App.jsx
@@ -0,0 +1,23 @@
+
+import Navbar from './Navbar/Navbar'
+import './App.css'
+import Hero from './Hero/Hero.jsx'
+
+
+
+
+function App() {
+
+  return (
+    <>
+    
+    <Navbar/>
+    <Hero/>
+ 
+
+    
+    </>
+  )
+}
+
+export default App
