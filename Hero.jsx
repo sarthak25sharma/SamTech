77bcb173a24fa537fa8df6660e20aafb8e6862f4
sarthak25sharma <155518490+sarthak25sharma@users.noreply.github.com>
2024-04-01 18:17:01 +0530

    Update Hero.jsx

--- Hero.jsx
+++ Hero.jsx
@@ -3,10 +3,14 @@ function Hero () {
 
     return(
         <>
-            <div className="hero-image">
-                <img  className="heroimg" src="./public/Hero.jpg" alt="samtech"></img>
-                <h1 className="text-overlay">WE ARE SAMTECH</h1>
-            </div>    
+            <section className="hero">
+                <div className="hero-inner">
+                    <h1>WELCOME TO SAMTECH</h1>
+                    <h2>LOREM IMPSUM KODEM!</h2>
+                    
+                </div>
+            </section>
+            
         </>
     );
 }
