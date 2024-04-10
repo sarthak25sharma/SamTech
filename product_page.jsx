7538f1b5d7309ae734cc150b03bff96a2a037037
Sarthak Sharma <155518490+sarthak25sharma@users.noreply.github.com>
2024-04-10 16:15:19 +0530

    Update product_page.jsx

--- product_page.jsx
+++ product_page.jsx
@@ -1,66 +1,226 @@
 import React from 'react';
 import "./product_page.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function ProductSection() {
   return (
-    
-    <div className="row">
-      <div className='heading-div'>
-            <hr/>
-            <h1 className='heading'>PLACEHOLDER</h1>
-            <hr/>
-      </div>
-  
-      <div className= "mb-4 bg-body-tertiary rounded-3 product-text row featurette flex-md-row-reverse">
-          <div className="col-md-7">
-            <h2 className=" f-normal featurette-heading fw-normal lh-1">Featurette heading.<span className="text-muted">It'll blow your mind.</span></h2>
-            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eum repellat similique molestiae sint quis esse provident dicta, perspiciatis magnam aperiam quasi, iste laborum, alias aliquam natus sed tenetur. Fugiat..</p>
-          </div>
-          <div className=" col-md-5">
-            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  src="./Hero.jpg" alt="" >
-            </img>
-          </div>
-      </div>
-      <div className="row align-items-md-stretch">
-          <div className="col-md-6">
-            <div className="h-100 p-4 text-bg-dark rounded-3">
-              <h2>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, explicabo debitis corporis praesentium iste cumque repellat hic placeat facilis nulla maxime illo perspiciatis. Autem impedit voluptates corporis ut consectetur quibusdam.
-              </h2>
+    <div className='container'>
+      <h1 className='main-heading'> Aluminium  & Copper wound distribution</h1>
+      <div className='main-products'>
+        <div className='card-div'>
+        <div className="card mb-3" >
+          <div className="row g-0">
+            <div className="image-col">
+              <img src="./Hero.jpg" className="img-fluid " alt="..." />
+            </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h2 className="card-title">Card title</h2>
+              <p className="card-textm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias ab dolore perferendis similique iste possimus cupiditate modi dignissimos, aliquid et, pariatur, animi alias facere id quisquam excepturi. Aperiam, doloribus voluptas..</p>
+              <p className="card-textm"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
           </div>
+        </div>
+        </div>
+      </div>
+      <div className='ratings'>
         
-          <div className="col-md-6">
-            <div className="h-100 p-5 bg-body-tertiary border rounded-3">
-              <h2>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima labore non quo pariatur deserunt accusantium, libero corporis odit velit neque placeat id illo quia praesentium vel, hic sequi inventore iste.
-              </h2>
+          <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+          <h1>Ratings of Transformer supported by us</h1>
+          <div className="list-group">
+            <label className="list-group-item d-flex gap-2">
+              <span>
+                16KVA
+                <small className="d-block text-body-secondary">With support text underneath to add more detail</small>
+              </span>
+            </label>
+            <label className="list-group-item d-flex gap-2">
               
+              <span>
+                25 KVA
+                <small className="d-block text-body-secondary">Some other text goes here</small>
+              </span>
+            </label>
+            <label className="list-group-item d-flex gap-2">
+              
+              <span>
+                63 KVA
+                <small className="d-block text-body-secondary">And we end with another snippet of text</small>
+              </span>
+            </label>
+            <label className="list-group-item d-flex gap-2">
+              
+              <span>
+                100KVA
+                <small className="d-block text-body-secondary">And we end with another snippet of text</small>
+              </span>
+            </label>
+          </div>
+          
+          <div className="list-group">
+            <label className="list-group-item d-flex gap-2">
+              
+              <span>
+                160 KVA
+                <small className="d-block text-body-secondary">With support text underneath to add more detail</small>
+              </span>
+            </label>
+            <label className="list-group-item d-flex gap-2">
+              
+              <span>
+                250 KVA
+                <small className="d-block text-body-secondary">Some other text goes here</small>
+              </span>
+            </label>
+            <label className="list-group-item d-flex gap-2">
+              
+              <span>
+                315 KVA
+                <small className="d-block text-body-secondary">And we end with another snippet of text</small>
+              </span>
+            </label>
+            <label className="list-group-item d-flex gap-2">
+              <span>
+                400 KVA
+                <small className="d-block text-body-secondary">And we end with another snippet of text</small>
+              </span>
+            </label>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+      
+      <div>
+        <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+        <div className="col-md-6 p-lg-5 mx-auto my-5">
+          <h1 className="display-3 fw-bold">other products</h1>
+          <h3 className="fw-normal text-muted mb-3">Build anything you want with Aperture</h3>
+          <div className="d-flex gap-3 justify-content-center lead fw-normal">
+            
+          </div>
+        </div>
+        <div className="product-device shadow-sm d-none d-md-block"></div>
+        <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+      </div>
+      </div>
+
+      <div className='other-products'>
+        <div class="album py-5 ">
+          <div class="container">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h3>Variable Voltage </h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center"> 
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                <h3>Industrial Transformer</h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                
+                <div class="card-body">
+                <h3>Oil Cooled Transformer</h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center">
+                    
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card shadow-sm">
+                
+                <div class="card-body">
+                <h3>Variable Auto </h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                
+                <div class="card-body">
+                <h3>Isolation Transformer</h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+                
+                <div class="card-body">
+                <h3>Control Transformer </h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card shadow-sm">
+                
+                <div class="card-body">
+                <h3>Three Phase Isolation </h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+              
+                <div class="card-body">
+                <h3>Sinlge Phase Isolation </h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card shadow-sm">
+              
+                <div class="card-body">
+                <h3>Dry air cooled</h3>
+                  
+                  <div class="d-flex justify-content-between align-items-center">
+                    
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
       </div>
-        <div className= " product-text row featurette flex-md-row-reverse">
-          
-          <div className="col-md-5">
-            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  src="./Hero.jpg" alt="">
-            </img>
-          </div>
-          <div className="col-md-7">
-            <h2 className=" f-normal featurette-heading fw-normal lh-1">Featurette heading. <span className="text-muted">It'll blow your mind.</span></h2>
-            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eum repellat similique molestiae sint quis esse provident dicta, perspiciatis magnam aperiam quasi, iste laborum, alias aliquam natus sed tenetur. Fugiat..</p>
-          </div>
-        </div>
-        <div className= " product-text row featurette flex-md-row-reverse">
-          <div className="col-md-7">
-            <h2 className=" f-normal featurette-heading fw-normal lh-1">Featurette heading. <span className="text-muted">It'll blow your mind.</span></h2>
-            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eum repellat similique molestiae sint quis esse provident dicta, perspiciatis magnam aperiam quasi, iste laborum, alias aliquam natus sed tenetur. Fugiat..</p>
-          </div>
-          <div className="col-md-5">
-            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  src="./Hero.jpg" alt="">
-            </img>
-          </div>
-        </div>
-       
     </div>
+  </div>
     
   );
 }
