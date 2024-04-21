37d54ba77fd41916cfe80d97d9199fcc0dfdef12
Sarthak Sharma <155518490+sarthak25sharma@users.noreply.github.com>
2024-04-21 09:45:51 +0530

    Update product_page.jsx

--- product_page.jsx
+++ product_page.jsx
@@ -91,12 +91,10 @@ function ProductSection() {
 
       
       <div>
-        <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
-        <div className="col-md-6 p-lg-5 mx-auto my-5">
-          <h1 className="display-3 fw-bold">other products</h1>
-          <h3 className="fw-normal text-muted mb-3">Build anything you want with Aperture</h3>
+        <div className="position-relative overflow-hidden  text-center bg-body-tertiary">
+        <div className=" other-image">
+          <h1 className="display-1 fw-bold">Other Products</h1>
           <div className="d-flex gap-3 justify-content-center lead fw-normal">
-            
           </div>
         </div>
         <div className="product-device shadow-sm d-none d-md-block"></div>
