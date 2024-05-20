e97094f9f335dc48e6f854bbb0f75f40588e91f6
Sarthak Sharma <155518490+sarthak25sharma@users.noreply.github.com>
2024-05-20 14:34:52 +0530

    Added heros

---
+++ sam/src/hero_infra.jsx
@@ -0,0 +1,17 @@
+import "./Hero.css"
+function Hero_Infra () {
+
+    return(
+        <>
+            <section className="hero">
+                <div className="hero-inner">
+                    <h1>OUR INFRASTRUCTURE</h1>
+                    <h2>LOREM IMPSUM KODEM!</h2>
+                    
+                </div>
+            </section>
+            
+        </>
+    );
+}
+export default Hero_Infra;
---
+++ sam/src/hero_products.jsx
@@ -0,0 +1,17 @@
+import "./Hero.css"
+function Hero_Products () {
+
+    return(
+        <>
+            <section className="hero">
+                <div className="hero-inner">
+                    <h1>OUR PRODUCTS</h1>
+                    <h2>LOREM IMPSUM KODEM!</h2>
+                    
+                </div>
+            </section>
+            
+        </>
+    );
+}
+export default Hero_Products;
